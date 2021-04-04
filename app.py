168e6bd6e4dad80d7a9245673ef8ee25d168dd33
aschultz10 <alexeschultz@gmail.com>
2021-04-03 20:14:42 -0400

    finish start/end routes

--- app.py
+++ app.py
@@ -6,15 +6,99 @@ from sqlalchemy import create_engine, func, inspect
 from flask import Flask, jsonify
 import datetime as dt
 
+
 # Database Setup
+    engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={'check_same_thread': False}, echo=True)
 
-engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={'check_same_thread': False}, echo=True)
+    Base = automap_base()
 
-Base = automap_base()
+    Base.prepare(engine, reflect=True)
 
-Base.prepare(engine, reflect=True)
+    Measurement = Base.classes.measurement
+    Station = Base.classes.station
 
-Measurement = Base.classes.measurement
-Station = Base.classes.station
+    session = Session(engine)    
+###------------------------------------------###
 
-session = Session(engine)
+
+
+# Flask Setup and Routes
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+###------------------------------------------###
+
+
+# Precipitation
+
+import datetime as dt
+
+def precipitation():
+    
+    latestdate = dt.date(2017, 8 ,23)
+    oneyearago = maxDate - dt.timedelta(days=365)
+    precquery = (session.query(Measurement.date, Measurement.prcp).filter(Measurement.date <= latestdate)
+                .filter(Measurement.date >= oneyearago).order_by(Measurement.date).all())
+
+    return jsonify(precquery)
+###------------------------------------------###
+
+
+# Stations
+
+@app.route('/api/v1.0/stations')
+def stations():
+
+    stationvar = session.query(Station.station).all()
+    return jsonify(stationvar)
+###------------------------------------------###
+
+
+# Tobs
+
+def tobs():  
+    maxDate = dt.date(2017, 8 ,23)
+    oneyearago = maxDate - dt.timedelta(days=365)
+
+    latestyear = (session.query(Measurement.tobs)
+                .filter(Measurement.station == 'USC00519281')
+                .filter(Measurement.date <= maxDate)
+                .filter(Measurement.date >= oneyearago)
+                .order_by(Measurement.tobs).all())
+    
+    return jsonify(latestyear)
+###------------------------------------------###
+
+
+# Start
+
+@app.route('/api/v1.0/<start>') 
+def start(start=None):
+    
+    tobs = (session.query(Measurement.tobs).filter(Measurement.date.between(start, '2017-08-23')).all())
+    
+    tobsdf = pd.DataFrame(tobs)
+    tavg = tobsdf["tobs"].mean()
+    tmax = tobsdf["tobs"].max()
+    tmin = tobsdf["tobs"].min()
+    
+    return jsonify(tmin, tmax, tavg)
+###------------------------------------------###
+
+# Start and End
+
+@app.route('/api/v1.0/<start>/<end>') 
+def startend(start=None, end=None):
+    
+    tobs = (session.query(Measurement.tobs).filter(Measurement.date.between(start, end)).all())
